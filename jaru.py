55cd8e529dc4067af0ad5b9d3bd93c2d52860be0
Ruller1 <88683626+Ruller1@users.noreply.github.com>
2022-01-29 13:20:57 +0000

    Add files via upload

--- jaru.py
+++ jaru.py
@@ -15,6 +15,7 @@ from colorama import init,Fore,Style
 import time
 import signal
 
+
 def def_handler(sig, frame):
     print(f'{Fore.RED}\n[-]Exit')
     sys.exit(1)
@@ -31,8 +32,8 @@ print(f'{Fore.CYAN}| |_| | (_| | |  | |_| |')
 time.sleep(0.1)
 print(f'{Fore.BLUE} \___/ \__,_|_|   \___/ \n')
 time.sleep(0.1)
-print(f'{Fore.RED}[!]EJECUTAR COMO ROOT')
-VictimIP = input(f"{Fore.CYAN}\n[+]Introduce la IP victima: ")
+print(f'{Fore.RED}\n[!]\nEJECUTAR COMO ROOT')
+victimIP = input(f"{Fore.CYAN}\n[+]Introduce la IP victima: ")
 print(f'\n{Fore.MAGENTA}[+]Modos de escaneo (1 al 5)')
 time.sleep(0.1)
 print(f'{Fore.RED}1.- Muy Agresivo || Velocidad:Rapido')
@@ -44,9 +45,11 @@ time.sleep(0.1)
 print(f'{Fore.GREEN}4.- Leve || Velocidad:Media Baja')
 time.sleep(0.1)
 print(f'{Fore.GREEN}5.- Muy Leve || Velocidad:Baja')
-ScanMode = int(input(f'{Fore.CYAN}\n[+]Elige tu modo de escaneo: '))
+
+scanMode = int(input(f'{Fore.CYAN}\n[+]Elige tu modo de escaneo: '))
 print(f'{Fore.MAGENTA}\n[+]Formatos')
 time.sleep(0.1)
+
 print(f'{Fore.RED}1.- Formato Grepeable || extractPorts para copiar puertos')
 time.sleep(0.1)
 print(f'{Fore.YELLOW}2.- Formato Normal || cat de toda la vida para leer')
@@ -56,104 +59,56 @@ time.sleep(0.1)
 print(f'{Fore.BLUE}4.- s4L1d4 sCRiPt KiDDi3 || No me la conteiner que sos Script Kiddie')
 time.sleep(0.1)
 print(f'{Fore.YELLOW}5.- No quiero exportar una pija || No exportar')
-Formato = input(f'{Fore.CYAN}\n[+]¿En qué formato deseas exportar?: ')
+
+formato = input(f'{Fore.CYAN}\n[+]¿En qué formato deseas exportar?: ')
 
 ###-Funciones-###
-def muyagresivogrepexport():
-    if Formato == 1:
-        muyagresivo() + os.system(f'-oG allports')
-
-
-
 
 def conect():
-    ping = os.system(f'ping -c 1 {VictimIP} >/dev/null 2>&1')
+    ping = os.system(f'ping -c 1 {victimIP} >/dev/null 2>&1')
     if ping == 0:
         return True
     else:
         return False
 
-def muyagresivo():
-    if Formato == 1:
-        os.system(f'nmap -p- --open -sS --min-rate 5000 -sCV -n -Pn {VictimIP} -oG allports')
-    elif Formato == 2:
-        os.system(f'nmap -p- --open -sS --min-rate 5000 -sCV -n -Pn {VictimIP} -oN allports')
-    elif Formato == 3:
-        os.system(f'nmap -p- --open -sS --min-rate 5000 -sCV -n -Pn {VictimIP} -oX allports')
-    elif Formato == 4:
-        os.system(f'nmap -p- --open -sS --min-rate 5000 -sCV -n -Pn {VictimIP} - allports') ##-Nose el formato script Kiddie XDDDD-##
-    elif Formato == 5:
-        os.system(f'nmap -p- --open -sS --min-rate 5000 -sCV -n -Pn {VictimIP}')
+def muyagresivogrep():
+        os.system(f'nmap -p- --open -sS --min-rate 5000 -sCV -n -Pn {victimIP} -oG allports')
 
 
-
-def agresivo():
-    if Formato == 1:
-        os.system(f'nmap -p- -sS --open -T5 -sCV -n -Pn {VictimIP} -oG allports')
-    elif Formato == 2:
-        os.system(f'nmap -p- -sS --open -T5 -sCV -n -Pn {VictimIP} -oN allports')
-    elif Formato == 3:
-        os.system(f'nmap -p- -sS --open -T5 -sCV -n -Pn {VictimIP} -oX allports')
-    elif Formato == 4:
-        os.system(f'nmap -p- -sS --open -T5 -sCV -n -Pn {VictimIP} Sk- allports') ##
-    elif Formato == 5:
-        os.system(f'nmap -p- -sS --open -T5 -sCV -n -Pn {VictimIP}')
+def agresivogrep():
+        os.system(f'nmap -p- -sS --open -T5 -sCV -n -Pn {victimIP} -oG allports')
+    
+       
 
 
-def tranquilomedio():
-    if Formato == 1:
-        os.system(f'nmap -p- -sS --open -sV -T3 -n -Pn {VictimIP} -oG allports')
-    elif Formato == 2:
-        os.system(f'nmap -p- -sS --open -sV -T3 -n -Pn {VictimIP} -oN allports')
-    elif Formato == 3:
-        os.system(f'nmap -p- -sS --open -sV -T3 -n -Pn {VictimIP} -oX allports')
-    elif Formato == 4:
-        os.system(f'nmap -p- -sS --open -sV -T3 -n -Pn {VictimIP} Sk- allports') ##
-    elif Formato == 5:
-        os.system(f'nmap -p- -sS --open -sV -T3 -n -Pn {VictimIP}')
+def tranquilomediogrep():
+        os.system(f'nmap -p- -sS --open -sV -T3 -n -Pn {victimIP} -oG allports')
+   
 
-
-def muytranquilo():
-    if Formato == 1:
-        os.system(f'nmap -T1 -sS --open -sV -n -Pn {VictimIP} -oG allports')
-    elif Formato == 2:
-        os.system(f'nmap -T1 -sS --open -sV -n -Pn {VictimIP} -oN allports')
-    elif Formato == 3:
-        os.system(f'nmap -T1 -sS --open -sV -n -Pn {VictimIP} -oX allports')
-    elif Formato == 4:
-        os.system(f'nmap -T1 -sS --open -sV -n -Pn {VictimIP} Sk- allports') ##
-    elif Formato == 5:
-        os.system(f'nmap -T1 -sS --open -sV -n -Pn {VictimIP}')
+def muytranquilogrep():
+        os.system(f'nmap -T1 -sS --open -sV -n -Pn {victimIP} -oG allports')
+  
     
 
-def sigiloso():
-    if Formato == 1:
-        os.system(f'nmap -p- -sS --open -T0 -n -Pn {VictimIP} -oG allports')
-    elif Formato == 2:
-        os.system(f'nmap -p- -sS --open -T0 -n -Pn {VictimIP} -oN allports')
-    elif Formato == 3:
-        os.system(f'nmap -p- -sS --open -T0 -n -Pn {VictimIP} -oX allports')
-    elif Formato == 4:
-        os.system(f'nmap -p- -sS --open -T0 -n -Pn {VictimIP} Sk- allports') ##
-    elif Formato == 5:
-        os.system(f'nmap -p- -sS --open -T0 -n -Pn {VictimIP}')
+def sigilosogrep():
+        os.system(f'nmap -p- -sS --open -T0 -n -Pn {victimIP} -oG allports')
 
 
 if conect() == True:
-    print(f'{Fore.GREEN}[+]Conexión con {VictimIP} exitosa')
-    if ScanMode == 1:
+    print(f'{Fore.GREEN}[+]Conexión con {victimIP} exitosa')
+    if scanMode == 1:
         print(f"{Fore.RED}\nIniciando escaneo muy agresivo..., abstente a las consecuencias")
         muyagresivo()
-    elif ScanMode == 2:
+    elif scanMode == 2:
         print(f"{Fore.RED}\nIniciando escaneo agresivo..., ¿te gusta el ruido?, ¿eres un espartano?")
         agresivo()
-    elif ScanMode == 3:
+    elif scanMode == 3:
         print(f"{Fore.YELLOW}\nIniciando escaneo medio..., no te gusta tanto el ruido eh, ¿tienes miedo?")
         tranquilomedio()
-    elif ScanMode == 4:
+    elif scanMode == 4:
         print(f"{Fore.GREEN}\nIniciando escaneo tranquilo..., chicos.. vengan encontramos a un pusilánime")
         muytranquilo()
-    elif ScanMode == 5:
+    elif scanMode == 5:
         print(f"{Fore.GREEN}\nIniciando escaneo muy tranquilo..., ¿si eres tan cagón para que me usas? ;(")
         sigiloso()
     else:
@@ -163,8 +118,13 @@ else:
 
 ###-Funciones de exportado-###
 
-
-
-def muysgresivoformatonormal():
-    if Formato == 2:
-        muyagresivo() + os.system(f'-oG allports')
+if scanMode == 1 and formato == 1:
+    muyagresivogrep()
+elif scanMode == 2 and formato == 1:
+    agresivogrep()
+elif scanMode == 3 and formato == 1:
+    tranquilomediogrep()
+elif scanMode == 4 and formato == 1:
+    muytranquilogrep()
+elif scanMode == 5 and formato == 1:
+    sigilosogrep()
